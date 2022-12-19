d20bf1d347c1a120b2f73e08213eb98c55d8313d
asamiul101 <58337911+samiularnob12@users.noreply.github.com>
2022-12-19 18:36:49 -0500

    remove unnecessary files in app.js

--- ASL-Learner/src/App.js
+++ ASL-Learner/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
 import Home from './home';
-import About from './about';
-import Project from './project';
 import {Routes, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Choosehand from './choosehand';
-import Webcams from './webcams';
 import ChooseLevel from './chooseLevel'
 import Game from './game'
 import Login from './Login';
@@ -17,7 +13,6 @@ import { useState } from 'react';
 function App() {
   const [email, setUser] = useState('');
   const [password, setPwd] = useState('');
-  //const [username, pass] = useState(null);
 
   return (
     <div className="App">
@@ -29,10 +24,6 @@ function App() {
       `}</style>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About />} />
-        <Route path= "/choosehand" element={<Choosehand />}/>
-        <Route path= "/webcams" element = {<Webcams />}/>
-        <Route path="/project" element={<Project />} />
         <Route path="/level" element={<ChooseLevel/>} />
         <Route path="/game" element={<Game />} />
         <Route path="/Login" element={<Login email={email} setUser={setUser} password={password} setPwd={setPwd}/>} />
